229e027befe664f3c0ba760b1cb9a7ff18719dea
Maikai1er <kolesnichenko.misha12@gmail.com>
2024-06-28 17:53:26 +0200

    added default reply and polling

--- la_baza_bot/telegram_bot.py
+++ la_baza_bot/telegram_bot.py
@@ -2,3 +2,24 @@ from telebot import TeleBot
 
 TOKEN = '7489778031:AAFW7ZxD4H3wQcQ4rMmSOOFFzY_-4vRCeMg'
 bot = TeleBot(TOKEN)
+
+
+@bot.message_handler(commands=['start'])
+def send_welcome(message):
+    first_name = message.from_user.first_name
+    last_name = message.from_user.last_name
+    full_name = f'{first_name} {last_name}' if last_name else first_name
+    bot.reply_to(message, f'Welcome, {full_name}!')
+
+
+@bot.message_handler(func=lambda message: '@la_baza_bot' in message.text)
+def reply(message):
+    bot.reply_to(message, text='You said "@la_baza_bot"')
+
+
+def run_telebot():
+    print('Running telebot...')
+    bot.polling()
+
+
+run_telebot()
